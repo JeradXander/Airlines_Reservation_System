1b2ae81991bcf912d6bb9f61d819af480fc554ce
Jerad Alexander <jeradal@amazon.com>
2021-04-27 09:15:49 -0700

    Working on Client, Airline, and flight

--- Airline_Reservation_Project/src/com/airline/Flights.java
+++ Airline_Reservation_Project/src/com/airline/Flights.java
@@ -10,26 +10,60 @@ import java.util.List;
 public class Flights {
 
     //fields
-   List<Ticket> tickets = new ArrayList<>();
+    private int flightId;
+    private List<Ticket> tickets = new ArrayList<>();
+    private Location origin;
+    private Location destination;
 
 
-   public Flights(){
-
-   }
+    public Flights(int flightIdArg,Location originArg, Location destinationArg) {
+        flightId = flightIdArg;
+        origin = originArg;
+        destination = destinationArg;
+    }
 
     public List<Ticket> getTickets() {
 
-       //instantiating lists
+        //instantiating lists
 
-        tickets.add(new Ticket(10,new EconomyTicket(new Location("Seattle", Timezone.PST), new Location("San francisco", Timezone.PST))));
-        tickets.add(new Ticket(10,new BusinessTicket(new Location("Seattle", Timezone.PST), new Location("San francisco", Timezone.PST))));
-        tickets.add(new Ticket(10,new FirstClassTicket(new Location("Seattle", Timezone.PST), new Location("San francisco", Timezone.PST))));
+        tickets.add(new Ticket(10, new EconomyTicket(origin, destination)));
+        tickets.add(new Ticket(10, new BusinessTicket(origin, destination)));
+        tickets.add(new Ticket(10, new FirstClassTicket(origin, destination)));
 
         return tickets;
     }
 
-    public void ticketBought(Ticket ticketBoughtArgs){
+    public void ticketBought(Ticket ticketBoughtArgs) {
         tickets.remove(ticketBoughtArgs);
     }
 
+    public int getFlightId() {
+        return flightId;
+    }
+
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public Location getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Location origin) {
+        this.origin = origin;
+    }
+
+    public Location getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Location destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(flightId);
+    }
 }

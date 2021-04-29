936df2fcc67b54f3c5cd948a56e38d33a0056959
Jerad Alexander <jeradal@amazon.com>
2021-04-29 10:41:31 -0700

    Finished Tests and Final push for deadline

--- Airline_Reservation_Project/test/com/client/AirlineClientTest.java
+++ Airline_Reservation_Project/test/com/client/AirlineClientTest.java
@@ -1,8 +1,13 @@
 package com.client;
 
 import com.airline.Airline;
+import com.airline.Flight;
 import com.locations.Location;
 import com.locations.Timezone;
+import com.tickets.Ticket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,6 +15,12 @@ import static org.junit.Assert.*;
 
 public class AirlineClientTest {
 
+    //initial fields for tests
+    Airline testAirline;
+    HashMap<String, List<Flight>> airlineFlightsExpected;
+    List<Flight> flightsListExpected;
+    Location origin;
+    Location destination;
     private Airline[] airlinesToChooseFrom = {
         new Airline("TLG Airlines"),
                 new Airline("Amazon Airlines"),
@@ -23,6 +34,7 @@ public class AirlineClientTest {
                 new Airline("Wheat Thins Airlines")
     };
 
+    //creating test locations
     Location[] locationsWithTimezone = {
             new Location("San Francisco", Timezone.PST),
             new Location("LA", Timezone.PST),
@@ -37,13 +49,68 @@ public class AirlineClientTest {
 
     @Before
     public void setup(){
+        //creating airline
+        testAirline = new Airline("TLG Airlines");
+        origin = new Location("San Diego", Timezone.PST);
+        destination = new Location("San Diego", Timezone.PST);
 
+        testAirline.setOrigin(origin);
+        testAirline.setDestination(origin);
+
+        //creating flights
+        flightsListExpected = new ArrayList<>();
+        flightsListExpected.add(new Flight(1000, origin, destination, "10:00 AM"));
+        flightsListExpected.add(new Flight(33000, origin, destination, "4:00 PM"));
+
+        //creating flights hashmap for testing
+        airlineFlightsExpected = new HashMap<>();
+        airlineFlightsExpected.put("04/29/2021", flightsListExpected);
+        airlineFlightsExpected.put("04/30/2021", flightsListExpected);
+        airlineFlightsExpected.put("05/01/2021", flightsListExpected);
+        airlineFlightsExpected.put("05/02/2021", flightsListExpected);
+        airlineFlightsExpected.put("05/03/2021", flightsListExpected);
+        airlineFlightsExpected.put("05/04/2021", flightsListExpected);
+        airlineFlightsExpected.put("05/05/2021", flightsListExpected);
     }
 
     @Test
     public void userSelectsAirlineFromList(){
         int userInput = 2;
+
         String expectedAirlineName = "Apprenti Airlines";
-        assertEquals(expectedAirlineName, airlinesToChooseFrom[2].getAirLineName());
+        assertEquals(expectedAirlineName, airlinesToChooseFrom[userInput].getAirLineName());
     }
+
+
+    @Test
+    public void userSelectsLocationNameFromList(){
+        //variable to test
+        int userInput = 2;
+        String expectedLocationName = "Seattle";
+
+        //assert to test
+        assertEquals(expectedLocationName, locationsWithTimezone[userInput].getName());
+    }
+
+    @Test
+    public void userSelectsDateFromList(){
+        //variable to test
+        String userInput = "04/29/2021";
+
+        //asssert to test
+        assertEquals(true   , testAirline.getAirlineFlights().containsKey(userInput));
+    }
+
+    @Test
+    public void userSelectsTicketFromList(){
+
+        //variables to test
+        String userdate = "04/29/2021";
+        String expectTicketString = "Flight Number: 33000-> Leaving at 4:00 PM";
+        int userInput = 1;
+
+        //assert for test
+        assertEquals( expectTicketString, testAirline.getAirlineFlights().get(userdate).get(userInput).toString());
+    }
+
 }
